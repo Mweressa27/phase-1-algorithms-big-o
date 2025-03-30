82a80967b8016f5738cde2f85f7d3f29684b5d45
Mweresa Esther Magoma <esthermweresa27@gmail.com>
2025-03-30 04:31:58 +0300

    complete tests

---
+++ index.js
@@ -0,0 +1,9 @@
+function hasTargetSum(array, target) {
+    const seenNumbers = new Set();
+    for (const number of array) {
+        const complement = target - number;
+        if (seenNumbers.has(complement)) return true;
+        seenNumbers.add(number)
+    }
+    return false;
+}
